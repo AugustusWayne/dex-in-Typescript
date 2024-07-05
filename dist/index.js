97c1e52a45886ffe2a4b6e045a1b63c72996de50
Ishaan Sharma <ishaansharma@Ishaans-Air.bbrouter>
2024-07-06 00:03:07 +0530

    init commit

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+let ETH_BALANCE = 200;
+let USDC_BALANCE = 700000;
+// app.post('/add-liquidity', (req, res) => {
+// })
+app.post('/buy-asset', (req, res) => {
+    const quantity = req.body.quantity;
+    const updatedEthQuantity = ETH_BALANCE - quantity;
+    const updatedUsdcBalance = ETH_BALANCE * USDC_BALANCE / updatedEthQuantity;
+    const paidamount = updatedUsdcBalance - USDC_BALANCE;
+    ETH_BALANCE = updatedEthQuantity;
+    USDC_BALANCE = updatedUsdcBalance;
+    res.json({
+        message: `You paid ${paidamount} USDC for ${quantity} ETH`
+
+    });
+    //   res.send(paidamount)
+});
+app.post('/sell-asset', (req, res) => {
+    const quantity = req.body.quantity;
+    const updatedEthQuantity = ETH_BALANCE + quantity;
+    const updatedUsdcBalance = ETH_BALANCE * USDC_BALANCE / updatedEthQuantity;
+    const gottenUSDC = USDC_BALANCE - updatedUsdcBalance;
+    ETH_BALANCE = updatedEthQuantity;
+    USDC_BALANCE = updatedUsdcBalance;
+    res.json({
+        message: `You got ${gottenUSDC} USDC for ${quantity} ETH`,
+    });
+});
+app.listen(3000);
+//# sourceMappingURL=index.js.map
